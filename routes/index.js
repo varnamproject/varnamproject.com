467b61b54f143574f9dafd05b2107c806f33f07a
navaneethkn <navaneethkn@gmail.com>
2012-08-22 19:02:10 +0530

    Adding express & restful URLs. Also hooked up learn

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var functions = require('../lib')
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  	res.render('index', { title: 'Express' });
+};
+
+app.get('/editor', function(req, res) {
+  	res.render('editor');
+});
+
+// /tl?lang=code&text=text_to_transliterate
+app.get('/tl', functions.tl);
+app.get('/rtl', functions.rtl);
+app.post('/learn', functions.learn);
---
+++ server.js
@@ -0,0 +1,77 @@
+#!/bin/env node
+//  OpenShift sample Node application
+
+var express = require('express');
+var fs      = require('fs');
+var http    = require('http')
+  , path    = require('path');
+
+//  Local cache for static content [fixed and loaded at startup]
+var zcache = { 'index.html': '' };
+zcache['index.html'] = fs.readFileSync('./index.html'); //  Cache index.html
+
+// Create "express" server.
+app  = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 8080);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+var routes  = require('./routes')
+
+app.get('/', routes.index);
+
+// Handler for GET /health
+app.get('/health', function(req, res){
+    res.send('1');
+});
+
+// Handler for GET /
+app.get('/', function(req, res){
+    res.send(zcache['index.html'], {'Content-Type': 'text/html'});
+});
+
+//  Get the environment variables we need.
+var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP;
+var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+
+if (typeof ipaddr === "undefined") {
+   console.warn('No OPENSHIFT_INTERNAL_IP environment variable');
+}
+
+//  terminator === the termination handler.
+function terminator(sig) {
+   if (typeof sig === "string") {
+      console.log('%s: Received %s - terminating Node server ...',
+                  Date(Date.now()), sig);
+      process.exit(1);
+   }
+   console.log('%s: Node server stopped.', Date(Date.now()) );
+}
+
+//  Process on exit and signals.
+process.on('exit', function() { terminator(); });
+
+['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS',
+ 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'
+].forEach(function(element, index, array) {
+    process.on(element, function() { terminator(element); });
+});
+
+//  And start the app on that interface (and port).
+app.listen(port, ipaddr, function() {
+   console.log('%s: Node server started on %s:%d ...', Date(Date.now() ),
+               ipaddr, port);
+});
+

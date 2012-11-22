0a4c9679c72959dab2c725b6d4f8428829ce8ba0
navaneethkn <navaneethkn@gmail.com>
2012-11-22 13:18:03 +0530

    Refactoring JS for addon part 6

--- browser/editor.js
+++ browser/editor.js
@@ -1,35 +1,33 @@
-(function () {
+(function() {
 
     window.Varnam = {};
 
-    var suggestionDivId         = 'popup',
-        suggestionDiv           = '#' + suggestionDivId,
-        suggestionList          = suggestionDiv + ' select',
-        suggestedItem           = suggestionDiv + suggestionList + ' option',
-        isSuggestionDisplayed   = false,
-        ignoreTextChange        = false,
-        timer                   = null,
+    var suggestionDivId = 'popup',
+        suggestionDiv = '#' + suggestionDivId,
+        suggestionList = suggestionDiv + ' select',
+        suggestedItem = suggestionDiv + suggestionList + ' option',
+        isSuggestionDisplayed = false,
+        ignoreTextChange = false,
+        timer = null,
         KEYS = {
-            ESCAPE:27,
-            ENTER:13,
-            TAB:9,
-            SPACE:32,
-            PERIOD:190,
-            DOWN_ARROW:40,
-            QUESTION:191,
-            EXCLAMATION:49,
-            COMMA:188,
-            LEFT_BRACKET:57,
-            RIGHT_BRACKET:48,
-            SEMICOLON:59
+            ESCAPE: 27,
+            ENTER: 13,
+            TAB: 9,
+            SPACE: 32,
+            PERIOD: 190,
+            DOWN_ARROW: 40,
+            QUESTION: 191,
+            EXCLAMATION: 49,
+            COMMA: 188,
+            LEFT_BRACKET: 57,
+            RIGHT_BRACKET: 48,
+            SEMICOLON: 59
         },
-        WORD_BREAK_CHARS        =  [KEYS.ENTER, KEYS.TAB, KEYS.SPACE,
-                                    KEYS.PERIOD, KEYS.QUESTION, KEYS.EXCLAMATION, KEYS.COMMA,
-                                    KEYS.LEFT_BRACKET, KEYS.RIGHT_BRACKET, KEYS.SEMICOLON],
-        myCodeMirror            = null,
-        textChangedCallback     = null,
-        lang                    = null,
-        errorCallback           = null;
+        WORD_BREAK_CHARS = [KEYS.ENTER, KEYS.TAB, KEYS.SPACE, KEYS.PERIOD, KEYS.QUESTION, KEYS.EXCLAMATION, KEYS.COMMA, KEYS.LEFT_BRACKET, KEYS.RIGHT_BRACKET, KEYS.SEMICOLON],
+        myCodeMirror = null,
+        textChangedCallback = null,
+        lang = 'en',
+        errorCallback = null;
 
     Varnam.init = function(options) {
         myCodeMirror = CodeMirror.fromTextArea(options.textArea, {
@@ -37,8 +35,8 @@
             lineNumbers: options.lineNumbers,
             lineWrapping: true,
             onChange: textChanged,
-            extraKeys:{
-                "Ctrl-Space":function (instance) {
+            extraKeys: {
+                "Ctrl-Space": function(instance) {
                     showSuggestion();
                 }
             },
@@ -64,17 +62,16 @@
     }
 
     function initialEventSetup() {
-        $("body").on('dblclick', suggestedItem, function () {
+        $("body").on('dblclick', suggestedItem, function() {
             replaceWordUnderCaret(getSelectedSuggestion());
             ignoreTextChange = true;
         });
 
-        $(suggestionList).keydown(function (event) {
+        $(suggestionList).keydown(function(event) {
             if (event.keyCode === KEYS.ESCAPE) {
                 hidePopup();
                 myCodeMirror.focus();
-            }
-            else if (isWordBreakKey(event.keyCode)) {
+            } else if (isWordBreakKey(event.keyCode)) {
                 var text = $(this).find(":selected").text();
                 if (text !== undefined && text !== '') {
                     replaceWordUnderCaret(text);
@@ -90,6 +87,7 @@
     }
 
     var to_replace_when_response_available = {};
+
     function replaceWordUnderCaret(text) {
         var w = getWordUnderCaret(myCodeMirror);
         var linech = w.start;
@@ -106,8 +104,8 @@
     function learnText(text) {
         if (lang === undefined || lang === 'en') return;
         $.post("learn", {
-            text:text,
-            lang:lang
+            text: text,
+            lang: lang
         });
     }
 
@@ -146,8 +144,7 @@
                 _event.preventDefault();
                 _event.stopPropagation();
                 return true;
-            }
-            else if (isWordBreakKey(_event.keyCode)) {
+            } else if (isWordBreakKey(_event.keyCode)) {
                 processWordBreaks();
                 if (_event.keyCode === KEYS.ENTER) {
                     _event.preventDefault();
@@ -156,13 +153,11 @@
                     return true;
                 }
             }
-        }
-        else if (_event.keyCode == KEYS.SPACE) {
+        } else if (_event.keyCode == KEYS.SPACE) {
             ignoreTextChange = true;
             var word = getWordUnderCaret(myCodeMirror);
             to_replace_when_response_available[word.word] = word;
-        }
-        else if (isWordBreakKey(_event.keyCode)) {
+        } else if (isWordBreakKey(_event.keyCode)) {
             ignoreTextChange = true;
         }
     }
@@ -177,66 +172,72 @@
     function showPopup(x, y, word) {
         if (lang === 'en') return;
         var params = {
-            'text':word,
-            'lang':lang
+            'text': word,
+            'lang': lang
         };
 
         show_error = false;
         hidePopup();
         request = $.ajax({
-            url:'tl?' + $.param(params),
-            dataType:'jsonp',
-            crossDomain:'true',
-            success:function (data) {
+            url: 'tl?' + $.param(params),
+            dataType: 'jsonp',
+            crossDomain: 'true',
+            success: function(data) {
                 if (errorCallback !== null) {
                     errorCallback(false);
                 }
-                html = "";
-                var textWidth = 0;
+
                 if (to_replace_when_response_available[data.input] !== undefined) {
                     wordToReplace = to_replace_when_response_available[data.input];
                     actualValueAtThatPos = myCodeMirror.getRange(wordToReplace.start, wordToReplace.end);
                     if (actualValueAtThatPos == data.input) {
-                        myCodeMirror.replaceRange (data.result[0], wordToReplace.start, wordToReplace.end);
+                        myCodeMirror.replaceRange(data.result[0], wordToReplace.start, wordToReplace.end);
                     }
                     delete to_replace_when_response_available[data.input];
-                }
-                else if (getWordUnderCaret(myCodeMirror).word == data.input) {
-                    $.each(data.result, function (index, value) {
+                } else if (getWordUnderCaret(myCodeMirror).word == data.input) {
+                    var html = "";
+                    var textWidth = 0;
+                    $.each(data.result, function(index, value) {
                         if (index === 0) {
                             html += '<option selected>' + value + '</option>';
-                        }
-                        else {
+                        } else {
                             html += '<option>' + value + '</option>';
                         }
                         if (textWidth < value.length) {
                             textWidth = value.length;
                         }
-                        $(suggestionList).html(html).css('width', (textWidth + 2) + 'em');
                     });
-                    $(suggestionList).html(html).css('height', (data.result.length+1) + 'em');
-                    var editor = $('.CodeMirror');
-                    var popup=$(suggestionDiv).css('display', "block").css('left', x + "px").css('top', (y + 15) + "px");
-                    var popupHeight = popup.height();
-                    var popupWidth = popup.width();
-                    if((y + popupHeight) > editor.position().top + editor.innerHeight()){
-                      popup.css('top',(y - popupHeight) + 'px');
-                    }
-                    if((x+popupWidth) > editor.position().left + editor.innerWidth()){
-                        popup.css('left',(x-popupWidth)+'px');
-                    }
+
+                    $(suggestionList).html(html).css('height', (data.result.length + 1) + 'em').css('width', (textWidth + 2) + 'em');
+                    positionPopup(x, y);
                     isSuggestionDisplayed = true;
                 }
             },
-            error: function(request, status, error) {
-                show_error = true;
-                window.setTimeout(function () {
-                    if (show_error && errorCallback !== null) {
-                        errorCallback(true);
-                    }
-                }, 2000);
-            }
+            error: requestFailed
         });
+    }
+
+    function requestFailed(request, status, error) {
+        show_error = true;
+        window.setTimeout(function() {
+            if (show_error && errorCallback !== null) {
+                errorCallback(true);
+            }
+        }, 2000);
+    }
+
+    // Popup will be placed according to the space available on the screen
+    function positionPopup(x, y) {
+        var editor = $('.CodeMirror');
+        var popup = $(suggestionDiv).css('display', "block").css('left', x + "px").css('top', (y + 15) + "px");
+        var popupHeight = popup.height();
+        var popupWidth = popup.width();
+        if ((y + popupHeight) > editor.position().top + editor.innerHeight()) {
+            popup.css('top', (y - popupHeight) + 'px');
+        }
+        if ((x + popupWidth) > editor.position().left + editor.innerWidth()) {
+            popup.css('left', (x - popupWidth) + 'px');
+        }
     }
 
     function hidePopup() {
@@ -245,10 +246,7 @@
     }
 
     function isWordBoundary(text) {
-        if (text === null || text === "" || text == " " || text == "\n" || text == "." || text == "\t" ||
-            text == "\r" || text == "\"" || text == "'" || text == "?" || text == "!" || text == "," ||
-            text == "(" || text == ")" || text == "\u000B" || text == "\u000C" || text == "\u0085" ||
-            text == "\u2028" || text == "\u2029" || text == "\u000D" || text == "\u000A" || text == ";") {
+        if (text === null || text === "" || text == " " || text == "\n" || text == "." || text == "\t" || text == "\r" || text == "\"" || text == "'" || text == "?" || text == "!" || text == "," || text == "(" || text == ")" || text == "\u000B" || text == "\u000C" || text == "\u0085" || text == "\u2028" || text == "\u2029" || text == "\u000D" || text == "\u000A" || text == ";") {
             return true;
         }
 
@@ -256,6 +254,7 @@
     }
 
     // Finds the word under caret and returns an object {start: {:line, :ch}, end: {:line, :ch}, :word}
+
     function getWordUnderCaret(editor) {
         var insertionPoint = editor.getCursor(); //{line, ch} object
         var startAt = 0;
@@ -267,19 +266,18 @@
         startAt = insertionPoint;
         while (caretPos) {
             text = editor.getRange({
-                line:insertionPoint.line,
-                ch:caretPos - 1
+                line: insertionPoint.line,
+                ch: caretPos - 1
             }, {
-                line:insertionPoint.line,
-                ch:caretPos
+                line: insertionPoint.line,
+                ch: caretPos
             });
             if (isWordBoundary(text)) {
                 break;
-            }
-            --caretPos;
+            }--caretPos;
             startAt = {
-                line:insertionPoint.line,
-                ch:caretPos
+                line: insertionPoint.line,
+                ch: caretPos
             };
 
         }
@@ -288,26 +286,25 @@
         caretPos = insertionPoint.ch;
         while (caretPos < lastPosition) {
             text = editor.getRange({
-                line:insertionPoint.line,
-                ch:caretPos
+                line: insertionPoint.line,
+                ch: caretPos
             }, {
-                line:insertionPoint.line,
-                ch:caretPos + 1
+                line: insertionPoint.line,
+                ch: caretPos + 1
             });
             if (isWordBoundary(text)) {
                 break;
-            }
-            ++caretPos;
+            }++caretPos;
             endsAt = {
-                line:insertionPoint.line,
-                ch:caretPos
+                line: insertionPoint.line,
+                ch: caretPos
             };
         }
 
         return {
-            start:startAt,
-            end:endsAt,
-            word:editor.getRange(startAt, endsAt)
+            start: startAt,
+            end: endsAt,
+            word: editor.getRange(startAt, endsAt)
         };
     }
 
@@ -316,7 +313,9 @@
         if (ignoreTextChange) {
             return;
         }
-        timer = window.setTimeout(function () { showSuggestion(); }, 10);
+        timer = window.setTimeout(function() {
+            showSuggestion();
+        }, 10);
         if (textChangedCallback !== null) {
             textChangedCallback(editor, from, to, text, next);
         }

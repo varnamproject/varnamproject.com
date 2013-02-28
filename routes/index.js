6164caa409a26a884cbdcc8fe2fbc188919d8db3
navaneethkn <navaneethkn@gmail.com>
2013-02-28 12:26:13 +0530

    WIP admin module

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,14 @@ exports.index = function(req, res){
 };
 
 app.get('/editor', function(req, res) {
-  	res.render('editor');
+    res.render('editor');
 });
 
 // /tl?lang=code&text=text_to_transliterate
 app.get('/tl', functions.tl);
 app.get('/rtl', functions.rtl);
 app.post('/learn', functions.learn);
+app.get('/admin', functions.admin);
+app.get('/admin/words_learned_last_days', functions.getLearnedWordsForLastDays);
+app.post('/admin/delete_word', functions.deleteWord);
+app.get('/admin/search_word', functions.searchWord);

3c1259e849216f79c3fa557c8fefdf6a086049fe
navaneethkn <navaneethkn@gmail.com>
2013-02-28 12:26:35 +0530

    Revert "Adding SQLite to package.json"
    This reverts commit d4755c36b8ba8804f41bab5ba81dd22b74e19168.

--- server.js
+++ server.js
@@ -23,10 +23,9 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
   app.use(express.session());
+  app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.csrf());
-  app.use(app.router);
 });
 
 var routes  = require('./routes')

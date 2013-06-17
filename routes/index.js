6340e0d4f4d2e99c1d7577dd6dd60646d1d3562b
navaneethkn <navaneethkn@gmail.com>
2013-06-17 12:34:36 +0530

    Adding supported languages URL

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,8 @@ app.get('/editor', function(req, res) {
   	res.render('editor');
 });
 
+app.get('/supported_languages', functions.supported_languages);
+
 // /tl?lang=code&text=text_to_transliterate
 app.get('/tl', functions.tl);
 app.get('/rtl', functions.rtl);
